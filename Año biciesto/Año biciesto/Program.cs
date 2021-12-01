d6925e6f402284f3b187d67073aeca9322f60622
Mjhon03 <Mjhon1841@gmail.com>
2021-12-01 13:32:03 -0500

    Algoritmos faltantes

---
+++ Año biciesto/Año biciesto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Año_biciesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algoritmo que determina si un año es bisiesto");
+            Console.WriteLine("Ingrese el año");
+            int year = int.Parse(Console.ReadLine());
+            bool esBisiesto = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+            if (esBisiesto)
+            {
+                Console.WriteLine("El año es bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("El año no es bisiesto");
+            }
+
+        }
+    }
+}
---
+++ Decimal a Binario/Decimal a Binario/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Decimal_a_Binario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Converti un numero decimal a binario");
+            Console.WriteLine("Introduzca el numero decimal");
+            int numeroDecimal = int.Parse(Console.ReadLine());
+            if (numeroDecimal > 0)
+            {
+                string cadena = "";
+                while (numeroDecimal > 0)
+                {
+                    if (numeroDecimal % 2 == 0)
+                    {
+                        cadena = "0" + cadena;
+
+                    }
+                    else
+                    {
+                        cadena = "1" + cadena;
+                    }
+                    numeroDecimal = (int)(numeroDecimal / 2);
+                }
+                Console.WriteLine(cadena);
+            }
+            else
+            {
+                if (numeroDecimal == 0)
+                {
+                    Console.WriteLine("0");
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese solo numeros positivos");
+                }
+            }
+        }
+    }
+}
